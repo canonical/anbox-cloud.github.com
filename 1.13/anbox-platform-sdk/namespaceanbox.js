cb2f3784c706b25d4d4f75c4e50df1d09a58bc2a
Simon Fels <simon.fels@canonical.com>
2022-02-28 08:30:52 +0100

    Add platform SDK docs for 1.13

---
+++ 1.13/anbox-platform-sdk/namespaceanbox.js
@@ -0,0 +1,12 @@
+var namespaceanbox =
+[
+    [ "AnboxProxy", "classanbox_1_1AnboxProxy.html", "classanbox_1_1AnboxProxy" ],
+    [ "AudioProcessor", "classanbox_1_1AudioProcessor.html", "classanbox_1_1AudioProcessor" ],
+    [ "CameraProcessor", "classanbox_1_1CameraProcessor.html", "classanbox_1_1CameraProcessor" ],
+    [ "GpsProcessor", "classanbox_1_1GpsProcessor.html", "classanbox_1_1GpsProcessor" ],
+    [ "GraphicsProcessor", "classanbox_1_1GraphicsProcessor.html", "classanbox_1_1GraphicsProcessor" ],
+    [ "InputProcessor", "classanbox_1_1InputProcessor.html", "classanbox_1_1InputProcessor" ],
+    [ "Platform", "classanbox_1_1Platform.html", "classanbox_1_1Platform" ],
+    [ "SensorProcessor", "classanbox_1_1SensorProcessor.html", "classanbox_1_1SensorProcessor" ],
+    [ "VideoDecoder", "classanbox_1_1VideoDecoder.html", "classanbox_1_1VideoDecoder" ]
+];
