d15e0971be411f1f071d698747d8d16ffd5475ce
Alexis Janon <alexis.janon@canonical.com>
2024-02-14 15:06:31 +0100

    Add API documentation for 1.21

---
+++ 1.21/anbox-platform-sdk/navtreedata.js
@@ -0,0 +1,91 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "anbox-platform-sdk", "index.html", [
+    [ "Anbox Platform SDK", "index.html", [
+      [ "Introduction", "index.html#introduction", null ],
+      [ "Overview", "index.html#overview", null ],
+      [ "Get Started", "index.html#get_started", null ]
+    ] ],
+    [ "API Overview", "anbox_sdk_api_overview_page.html", [
+      [ "Introduction", "anbox_sdk_api_overview_page.html#API", null ],
+      [ "Abstract classes", "anbox_sdk_api_overview_page.html#abstract_classes", [
+        [ "AudioProcessor", "anbox_sdk_api_overview_page.html#anbox_audio_processor", null ],
+        [ "InputProcessor", "anbox_sdk_api_overview_page.html#anbox_input_processor", null ],
+        [ "GraphicsProcessor", "anbox_sdk_api_overview_page.html#anbox_graphics_processor", null ],
+        [ "SensorProcessor", "anbox_sdk_api_overview_page.html#anbox_sensor_processor", null ],
+        [ "AnboxProxy", "anbox_sdk_api_overview_page.html#anbox_proxy", null ],
+        [ "GpsProcessor", "anbox_sdk_api_overview_page.html#anbox_gps_processor", null ],
+        [ "CameraProcessor", "anbox_sdk_api_overview_page.html#anbox_camera_processor", null ]
+      ] ]
+    ] ],
+    [ "Creating a Platform Plugin", "creating_a_platform_plugin.html", [
+      [ "Preparation", "creating_a_platform_plugin.html#preparation", null ],
+      [ "Create a CMake-based plugin project", "creating_a_platform_plugin.html#create_platform_plugin", null ],
+      [ "Build your platform plugin", "creating_a_platform_plugin.html#build_platform_plugin", null ],
+      [ "Test your platform plugin", "creating_a_platform_plugin.html#test_platform_plugin", null ]
+    ] ],
+    [ "Package and deploy a platform plugin", "anbox_sdk_plugin_package_and_deploy.html", [
+      [ "Package as AMS addon", "anbox_sdk_plugin_package_and_deploy.html#package_as_ams_addon", null ],
+      [ "Use platform in a container", "anbox_sdk_plugin_package_and_deploy.html#use_platform_in_a_container", null ]
+    ] ],
+    [ "Examples", "anbox_sdk_examples.html", "anbox_sdk_examples" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"anbox__proxy_8h.html",
+"public__api_8h.html#a04234860725f490f48e03580d6df9e1a",
+"structAnboxVhalConnectorCallbacks.html#a2d38b3b3454e27de27faa5e2694782c6",
+"types_8h.html#ad941286af23aaea1407072ec3e1a5174"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
