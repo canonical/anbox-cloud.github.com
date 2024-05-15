716a65ba6867630c2584dafe30970a4f8b92408b
Alexis Janon <alexis.janon@canonical.com>
2024-05-15 11:32:04 +0200

    Add API documentation for 1.22

---
+++ 1.22/anbox-platform-sdk/plugin_8h.js
@@ -0,0 +1,15 @@
+var plugin_8h =
+[
+    [ "AnboxAudioProcessor", "structAnboxAudioProcessor.html", "structAnboxAudioProcessor" ],
+    [ "AnboxInputProcessor", "structAnboxInputProcessor.html", "structAnboxInputProcessor" ],
+    [ "AnboxGraphicsProcessor", "structAnboxGraphicsProcessor.html", "structAnboxGraphicsProcessor" ],
+    [ "AnboxSensorProcessor", "structAnboxSensorProcessor.html", "structAnboxSensorProcessor" ],
+    [ "AnboxGpsProcessor", "structAnboxGpsProcessor.html", "structAnboxGpsProcessor" ],
+    [ "AnboxCameraProcessor", "structAnboxCameraProcessor.html", "structAnboxCameraProcessor" ],
+    [ "AnboxProxy", "structAnboxProxy.html", "structAnboxProxy" ],
+    [ "AnboxVideoDecoder", "structAnboxVideoDecoder.html", "structAnboxVideoDecoder" ],
+    [ "AnboxVhalConnector", "structAnboxVhalConnector.html", "structAnboxVhalConnector" ],
+    [ "AnboxPlatform", "structAnboxPlatform.html", "structAnboxPlatform" ],
+    [ "anbox_platform_plugin_register", "plugin_8h.html#ad04f9a53e4dc96a7d945da269443b6dd", null ],
+    [ "anbox_platform_plugin_unregister", "plugin_8h.html#afd9984492655871a85d7908e3d1f2d01", null ]
+];
