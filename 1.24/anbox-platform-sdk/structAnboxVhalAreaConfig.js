36f1da736f56dc9ed465dd349d9f0f448a9a3697
Alexis Janon <alexis.janon@canonical.com>
2025-02-12 16:12:48 +0100

    Add anbox-platform-sdk API documentation for 1.24

---
+++ 1.24/anbox-platform-sdk/structAnboxVhalAreaConfig.js
@@ -0,0 +1,12 @@
+var structAnboxVhalAreaConfig =
+[
+    [ "area_id", "structAnboxVhalAreaConfig.html#ac4db614d9e99e58b1da9542deafb30fd", null ],
+    [ "area_names", "structAnboxVhalAreaConfig.html#a502ce56cb2d95cb12881756b9fe7b2e2", null ],
+    [ "area_names_size", "structAnboxVhalAreaConfig.html#afd2978a2a097a8173d73b80c097e643e", null ],
+    [ "max_float_value", "structAnboxVhalAreaConfig.html#a5a43aa894b4ddeaf536d42b4fa3d090e", null ],
+    [ "max_int32_value", "structAnboxVhalAreaConfig.html#adc298c3a38d937ad18a25906d0dc4cfa", null ],
+    [ "max_int64_value", "structAnboxVhalAreaConfig.html#ab6025eb94bbefd75ac3560ffd50d5fdc", null ],
+    [ "min_float_value", "structAnboxVhalAreaConfig.html#a1adf6932aa64dc5c93c864247e06cd5d", null ],
+    [ "min_int32_value", "structAnboxVhalAreaConfig.html#a0067b0868160773ca8bd98c6bc30a23b", null ],
+    [ "min_int64_value", "structAnboxVhalAreaConfig.html#ab9077c448c31387feb0a4faf6fc4293d", null ]
+];
