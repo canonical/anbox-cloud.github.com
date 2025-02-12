fabc36daac24b07c6fa86a51460f7c800125be55
Alexis Janon <alexis.janon@canonical.com>
2025-02-12 16:14:04 +0100

    Add anbox-platform-sdk API documentation for 1.25

---
+++ 1.25/anbox-platform-sdk/structAnboxSensorVector.js
@@ -0,0 +1,12 @@
+var structAnboxSensorVector =
+[
+    [ "angle", "structAnboxSensorVector.html#abdea77bfb1eaea1abeaa2056668d78e6", null ],
+    [ "axis", "structAnboxSensorVector.html#af132769f0430aec391581327a4fcfb98", null ],
+    [ "azimuth", "structAnboxSensorVector.html#aa58d1a1f0ca07bd1c949f3d5851511be", null ],
+    [ "pitch", "structAnboxSensorVector.html#a120f07b6e4ae35d8f7ce18ac71f096f3", null ],
+    [ "roll", "structAnboxSensorVector.html#aa8451be8e809d5695c1bc78db2d34033", null ],
+    [ "v", "structAnboxSensorVector.html#a63e935ba3059e558aaa270adf933a757", null ],
+    [ "x", "structAnboxSensorVector.html#ae9e53d5ce6ad476fa5526e76acd3896f", null ],
+    [ "y", "structAnboxSensorVector.html#a13996a1978ab4116bfde6fa1bccafe7a", null ],
+    [ "z", "structAnboxSensorVector.html#a2f88f53ccb88b9d41f4315eeb9759295", null ]
+];
