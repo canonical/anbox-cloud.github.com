f3dc97b7f32eade49a7cc53d00fa8a9bf07e4e9a
gary-wzl77 <gary.wang@canonical.com>
2025-07-24 19:06:20 +0800

    feat(platform-sdk): add documentation for anbox-platform-sdk 1.26

---
+++ 1.26/anbox-platform-sdk/files_dup.js
@@ -0,0 +1,30 @@
+var files_dup =
+[
+    [ "anbox_proxy.h", "anbox__proxy_8h.html", "anbox__proxy_8h" ],
+    [ "audio_processor.h", "audio__processor_8h.html", [
+      [ "AudioProcessor", "classanbox_1_1AudioProcessor.html", "classanbox_1_1AudioProcessor" ]
+    ] ],
+    [ "camera_processor.h", "camera__processor_8h.html", [
+      [ "CameraProcessor", "classanbox_1_1CameraProcessor.html", "classanbox_1_1CameraProcessor" ]
+    ] ],
+    [ "common.h", "common_8h.html", "common_8h" ],
+    [ "gps_processor.h", "gps__processor_8h.html", [
+      [ "GpsProcessor", "classanbox_1_1GpsProcessor.html", "classanbox_1_1GpsProcessor" ]
+    ] ],
+    [ "graphics_processor.h", "graphics__processor_8h.html", "graphics__processor_8h" ],
+    [ "input_processor.h", "input__processor_8h.html", [
+      [ "InputProcessor", "classanbox_1_1InputProcessor.html", "classanbox_1_1InputProcessor" ]
+    ] ],
+    [ "platform.h", "platform_8h.html", "platform_8h" ],
+    [ "plugin.h", "plugin_8h.html", "plugin_8h" ],
+    [ "public_api.h", "public__api_8h.html", "public__api_8h" ],
+    [ "sensor_processor.h", "sensor__processor_8h.html", [
+      [ "SensorProcessor", "classanbox_1_1SensorProcessor.html", "classanbox_1_1SensorProcessor" ]
+    ] ],
+    [ "types.h", "types_8h.html", "types_8h" ],
+    [ "version.h", "version_8h.html", "version_8h" ],
+    [ "vhal_connector.h", "vhal__connector_8h.html", "vhal__connector_8h" ],
+    [ "video_decoder.h", "video__decoder_8h.html", [
+      [ "VideoDecoder", "classanbox_1_1VideoDecoder.html", "classanbox_1_1VideoDecoder" ]
+    ] ]
+];
