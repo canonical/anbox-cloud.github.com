0f6335e70f8aac88ee185c3ce5681c45a7337633
gary-wzl77 <gary.wang@canonical.com>
2025-07-24 19:06:20 +0800

    feat(platform-sdk): add documentation for anbox-platform-sdk 1.26

---
+++ 1.26/anbox-platform-sdk/classanbox_1_1Platform.js
@@ -0,0 +1,23 @@
+var classanbox_1_1Platform =
+[
+    [ "Platform", "classanbox_1_1Platform.html#a1734405780b905b5b3a14382fc8c6c70", null ],
+    [ "~Platform", "classanbox_1_1Platform.html#a73b8f6fb5b0f37b37ebd4633665b7f16", null ],
+    [ "Platform", "classanbox_1_1Platform.html#acf7e6be8a97b242e451f54a5b8e1bcdb", null ],
+    [ "anbox_proxy", "classanbox_1_1Platform.html#a16f6ea0ed2b2dc10f90b95887ef7ace5", null ],
+    [ "audio_processor", "classanbox_1_1Platform.html#a4ab51c5daf0c30f5e1cbca6fec1f1499", null ],
+    [ "camera_processor", "classanbox_1_1Platform.html#a1aedc580fcea297f287feb8b512a0f6d", null ],
+    [ "create_video_decoder", "classanbox_1_1Platform.html#a6478bd39fb07951c0e95903f42857089", null ],
+    [ "get_config_item", "classanbox_1_1Platform.html#a90ff17206a0fc0cb05cfdaf8caf6d782", null ],
+    [ "gps_processor", "classanbox_1_1Platform.html#a33ae2ef994dd4760e07af982b452be91", null ],
+    [ "graphics_processor", "classanbox_1_1Platform.html#a3455195c7b102a54647d4f1aec85618c", null ],
+    [ "handle_event", "classanbox_1_1Platform.html#a3c6d0ac26305e68bdb19e1c3bcaa528b", null ],
+    [ "input_processor", "classanbox_1_1Platform.html#a02ef565c2757d395fe86333175d3a2a8", null ],
+    [ "operator=", "classanbox_1_1Platform.html#a9a06cdaf2484f9dfe1426fee4f43a567", null ],
+    [ "ready", "classanbox_1_1Platform.html#a3cfb767fac35c8e355923f7d85327b9c", null ],
+    [ "sensor_processor", "classanbox_1_1Platform.html#af0ada09473c66da26efafa0828fe74d7", null ],
+    [ "set_config_item", "classanbox_1_1Platform.html#aa02d7d7b6900910b0913b3051e99313c", null ],
+    [ "setup_event_tracer", "classanbox_1_1Platform.html#ae51dc11ffd1d5c35c308b5da77c33c64", null ],
+    [ "stop", "classanbox_1_1Platform.html#af7a0fcf1b3d7f08a47b880215b63745c", null ],
+    [ "vhal_connector", "classanbox_1_1Platform.html#a8190dfd8862f8e8008e11cdb738b7000", null ],
+    [ "wait_until_ready", "classanbox_1_1Platform.html#a7d8fa1d14143a5af92de58deb780ea40", null ]
+];
