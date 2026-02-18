ad61d6582fe875a52590d5411af19f8e6ec8f836
Alexis Janon <alexis.janon@canonical.com>
2026-02-18 14:44:53 +0100

    Add anbox-platform-sdk API documentation for 1.29

---
+++ 1.29/anbox-platform-sdk/platform_8h.js
@@ -0,0 +1,5 @@
+var platform_8h =
+[
+    [ "Platform", "classanbox_1_1Platform.html", "classanbox_1_1Platform" ],
+    [ "ANBOX_PLATFORM_PLUGIN_DESCRIBE", "platform_8h.html#a18b834b14b05e5d31ab80b441c094247", null ]
+];
